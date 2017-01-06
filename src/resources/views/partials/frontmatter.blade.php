4a28d93a037e8a15641cea84020021f342516aac
Van Korn <vanderkorn@gmail.com>
2017-01-06 13:28:41 +0500

    fix

--- src/resources/views/partials/frontmatter.blade.php
+++ src/resources/views/partials/frontmatter.blade.php
@@ -1,12 +1,11 @@
 title: API Reference
 
 language_tabs:
-- bash
-- javascript
 
 includes:
 
 search: true
 
 toc_footers:
+- <a href='http://binomtech.com'>Created by BinomTech</a>
 - <a href='http://github.com/mpociot/documentarian'>Documentation Powered by Documentarian</a>
